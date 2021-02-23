44d15cd417968fd1e8f4c533df92bad8a58b21fe
Saratoga93 <70145001+Saratoga93@users.noreply.github.com>
2021-02-23 16:31:55 +0900

    java 2차원 배열 실습

---
+++ 강병수/201229/src/Array3.java
@@ -0,0 +1,23 @@
+
+public class Array3 {
+
+	public static void main(String[] args) {
+		int arr[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print(arr[i][j] + "\t");
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+		int[][] arr2 = { { 1, 2, 3 }, { 4, 5 }, { 6, 7, 8 }, { 9 } };
+		for (int i = 0; i < arr2.length; i++) {
+			for (int j = 0; j < arr2[i].length; j++) {
+				System.out.print(arr2[i][j] + "\t");
+			}
+			System.out.println("");
+		}
+	}
+
+}
